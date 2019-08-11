b5b5812c18d359432b9a563b6d7484b7291fb8c8
p.e.kolomnikov <p.e.kolomnikov@tinkoff.ru>
2019-08-11 18:01:26 +0700

    #22

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -9,8 +9,16 @@
 (my-nth '(4 5 6 7) 2)
 
 (= (my-nth '(4 5 6 7) 2) 6)
-
 (= (my-nth [:a :b :c] 0) :a)
-
 (= (my-nth [1 2 3 4] 1) 2)
 
+;Write a function which returns the total number of elements in a sequence.
+(defn count-seq
+  [arr]
+  (reduce (fn [acc _] (inc acc)) 0 arr))
+
+(count-seq '(1 2 3 4))
+(= (count-seq '(1 2 3 3 1)) 5)
+(= (count-seq "Hello World") 11)
+(= (count-seq '(13)) 1)
+(= (count-seq '(13)) 1)
